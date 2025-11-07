ccc3499489e28f6751dc9aa08557eecb9756346e
zch <15690413+Ikaros423@user.noreply.gitee.com>
2025-11-07 11:29:50 +0800

    完成evaluateReport模块及单元测试

--- src/main/java/com/course/coursedesign2022/controller/EvaluateReportController.java
+++ src/main/java/com/course/coursedesign2022/controller/EvaluateReportController.java
@@ -1,4 +1,22 @@
 package com.course.coursedesign2022.controller;
 
+import com.course.coursedesign2022.service.EvaluateReport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EvaluateReportController {
+    @Autowired
+    private EvaluateReport evaluateReport;
+
+    @GetMapping("/evaluateReport/{id}")
+    public String evaluateReport(@PathVariable Integer id){
+        boolean res = evaluateReport.evaluateReport(id);
+        if (res){
+            return "evaluateReport success";
+        }
+        return "evaluateReport failed";
+    }
 }
