c0475da8550d2b73845256583c854da628f0ff20
zch <15690413+Ikaros423@user.noreply.gitee.com>
2025-11-07 10:44:26 +0800

    完成login模块及单元测试

--- src/main/java/com/course/coursedesign2022/controller/LoginController.java
+++ src/main/java/com/course/coursedesign2022/controller/LoginController.java
@@ -1,4 +1,22 @@
 package com.course.coursedesign2022.controller;
 
+import com.course.coursedesign2022.service.Login;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginController {
+    @Autowired
+    private Login login;
+
+    @PutMapping("/login/{id}")
+    public String login(@PathVariable Integer id) {
+        boolean res = login.login(id);
+        if (res){
+            return "login success";
+        }
+        return "login failed";
+    }
 }
