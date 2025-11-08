b9e2792a4d15a254bcaf71e9084b851d20051213
zx <>
2025-11-08 13:50:39 +0800

    完成extendedActivity

--- src/main/java/com/course/coursedesign2022/service/ExtendedActivity.java
+++ src/main/java/com/course/coursedesign2022/service/ExtendedActivity.java
@@ -1,4 +1,27 @@
 package com.course.coursedesign2022.service;
 
+import com.course.coursedesign2022.mapper.userInfoMapper;
+import com.course.coursedesign2022.pojo.userInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ExtendedActivity {
+    @Autowired
+    private userInfoMapper userInfoMapper;
+
+    public boolean extendedActivity(int id) {
+        userInfo userInfo = userInfoMapper.selectByPrimaryKey(id);
+
+        if (userInfo == null) {
+            return false;
+        }
+
+        userInfo.setExchangescore(userInfo.getExchangescore() + 5);
+        userInfo.setScoretotal(userInfo.getGrowscore() + userInfo.getExchangescore());
+        userInfoMapper.updateByPrimaryKeySelective(userInfo);
+
+        System.out.println("extendedActivity execute");
+        return true;
+    }
 }
